903157b416a33814fe38e26eb242cfede296623f
Egorov Vlad <jefoce23@gmail.com>
2021-09-24 01:31:41 +0300

    UPDATE: Tags, readme, types

--- utils/create-context-hook.tsx
+++ utils/create-context-hook.tsx
@@ -1,19 +1,19 @@
 import { createContext, useContext } from "react";
 import { PascalCase, toPascalCase } from "./to-pascal-case";
 
-type HookFunc<Payload, Return> = void extends Payload ? () => Return : (payload: Payload) => Return;
-type Props<P> = void extends P ? { children?: React.ReactNode } : P & { children?: React.ReactNode };
+type HookFunc<P extends object, R extends object> = void extends P ? () => R : (payload: P) => R;
+type Props<P extends object> = void extends P ? { children?: React.ReactNode } : P & { children?: React.ReactNode };
 
 type HookName<Name extends string> = `use${PascalCase<Name>}`;
 type ProviderName<Name extends string> = `${PascalCase<Name>}Provider`;
 
-type Result<P, R, Name extends string> = {
+type Result<P extends object, R extends object, Name extends string> = {
   [K in HookName<Name> | ProviderName<Name>]: K extends HookName<Name>
     ? () => R
-    : <OutP = P>(props: Props<OutP>) => JSX.Element;
+    : <OutP extends object = P>(props: Props<OutP>) => JSX.Element;
 };
 
-export const createContextHook = <P, R, Name extends string>(name: Name, useHook: HookFunc<P, R>) => {
+export const createContextHook = <P extends object, R extends object, Name extends string>(name: Name, useHook: HookFunc<P, R>) => {
   const Context = createContext({} as R);
   const pascal = toPascalCase(name);
 
@@ -24,8 +24,8 @@ export const createContextHook = <P, R, Name extends string>(name: Name, useHook
 
   return {
     [hookName]: () => useContext(Context),
-    [providerName]: ({ children, ...props }: Props<P>) => (
-      <Context.Provider value={useHook(props as any)} children={children} />
+    [providerName]: ({ children, ...payload }: Props<P>) => (
+      <Context.Provider value={useHook(payload as P)} children={children} />
     ),
   } as Result<P, R, Name>;
 };
